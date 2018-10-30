f7e33bf75acdbb2d151ccf3324225726b99d4efc
weechang93 <zhangwei_sc@foxmail.com>
2018-10-30 13:44:18 +0800

    deploy at `date +"%Y-%m-%d %H:%M:%S"`

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,6 @@
 cd docs/
-git push --force --quiet "https://weechang93:Zw123456coding@git.coding.net/weechang93/moreco-doc.git" master:master
+git config user.name "weechang93"
+git config user.email "zhangwei_sc@foxmail.com"
+git add -A
+git commit -m "deploy at `date +\"%Y-%m-%d %H:%M:%S\"`"
+git push --force --quiet "https://weechang93:${CI_TOKEN_CODING}@git.coding.net/weechang93/moreco-doc.git" master:master
